8be2bf3cae74563d4ac35605cfb8904a011b5dbd
MariusArhaug <mariusarhaug@hotmail.com>
2022-02-03 23:15:26 +0100

    FEAT: create clock struct

--- src/clock.c
+++ src/clock.c
@@ -1,16 +1,34 @@
 #include "clock.h"
+#include <stdlib.h>
 
-void schedule(char time) 
-{
-
-}
-
-void list()
-{
-
-}
-
-void cancel() 
+alarm_clock_t* initialize()
 {
   
+  alarm_clock_t* clock = malloc(sizeof(alarm_clock_t));
+
+  clock->capacity = INIT_SIZE;
+  clock->length = 0;
+  clock->alarms = malloc(clock->length * sizeof(alarm_t));
+  return clock;
+}
+
+
+void push(alarm_clock_t* clock, alarm_t alarm)
+{
+  // Need to reallocate memory if array has reached max capcaity. 
+  if (clock->length == clock->capacity)
+  {
+    clock->capacity *= 2;
+    clock->alarms = realloc(clock->alarms, sizeof(alarm_t) * clock->capacity);
+  }
+
+  clock->alarms[clock->length++] = alarm;
+}
+
+void free_clock(alarm_clock_t* clock) 
+{
+  free(clock->alarms);
+  clock->alarms = NULL;
+  clock->length = clock->capacity = 0;
+  free(clock);
 }
--- src/clock.h
+++ src/clock.h
@@ -1,8 +1,27 @@
 #ifndef CLOCK_H
 #define CLOCK_H
 
-void schedule(char);
-void list(void);
-void cancel(void);
+#define __USE_XOPEN
+#define __GNU_SOURCE
+#include "time.h"
+
+#define INIT_SIZE 10
+
+typedef struct {
+  time_t time;
+} alarm_t ;
+
+typedef struct  {
+  alarm_t *alarms;
+  int capacity;
+  int length;
+} alarm_clock_t;
+
+
+alarm_clock_t* initialize(void);
+
+void push(alarm_clock_t *, alarm_t);
+
+void free_clock(alarm_clock_t *);
 
 #endif
